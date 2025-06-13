c9f81d08f0197cd1dbe86fe43096e0ce9d390fb0
JobMutugi <creativejobu@gmail.com>
2025-06-13 09:35:51 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = { name: "Sam", streetAddress: "11 Broadway" };
+
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  return { ...emp, [key]: value };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  emp[key] = value; return emp;
+}
+
+
+function deleteFromEmployeeByKey(emp, key) {
+  let newEmp = { ...emp }; delete newEmp[key]; return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+  delete emp[key]; return emp;
+}
